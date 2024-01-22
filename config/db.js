9be79946f90f88d5b32bdd4475ad05f6ba02c0db
Piyarilal Chingtham <khumanchingtham123@gmail.com>
2024-01-22 06:27:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('process.env.MONGO_URL');
+    await mongoose.connect('mongodb+srv://khumanchingtham123:gLLpA47IIW6BxiUp@cluster0.9pqxmex.mongodb.net/?retryWrites=true&w=majority');
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
